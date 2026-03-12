26fbe002b8914fe5e6e84affc68a2c692252672f
Boss Porsche <sophearithykuy@gmail.com>
2026-03-12 15:49:42 +0700

    Refactor sales_router with updated options and imports

--- POS/sales_router.py
+++ POS/sales_router.py
@@ -1,23 +1,26 @@
-from Sales import Sale
-from dashboard import show_charts
+from sales import Sale
+from chart_analysis import show_charts
 sale = Sale()
-
 while True:
-
-    print("\n===== PHARMACY POS =====")
+    print("\n===== PHARMACY POS SYSTEM =====")
     print("1. Make Sale")
     print("2. View Sales")
-    print("3. Chart Analysis")
-    print("4. Exit")
+    print("3. Search Sale")
+    print("4. Delete Sale")
+    print("5. Chart Analysis")
+    print("6. Exit")
     choice = input("Choose option: ")
     if choice == "1":
         sale.make_sale()
     elif choice == "2":
         sale.view_sales()
     elif choice == "3":
-        show_charts()
+        sale.search_sale()
     elif choice == "4":
+        sale.delete_sale()
+    elif choice == "5":
+        show_charts()
+    elif choice == "6":
         break
     else:
-        print("Invalid option!")
-        
+        print("Invalid option. Try again.")
