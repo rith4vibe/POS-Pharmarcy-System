4049649b9947b438ff2582c58b21fe805f857da8
Boss Porsche <sophearithykuy@gmail.com>
2026-03-14 00:30:34 +0700

    Update main_router.py

--- Pos_Pharmacy/main_router.py
+++ Pos_Pharmacy/main_router.py
@@ -1,40 +1,36 @@
 from management_medicine import Manage_Medicine
 from sales import Sale
-
-def main_router():
-    sale = Sale()
+def login():
     username = "admin"
-    password = 112233
-    
+    password = "112233"
     while True:
-        print("============== PLEASE LOGIN ===============")
-
+        print("\n============== PLEASE LOGIN ===============")
         u = input("Enter Username: ")
-        p = int(input("Enter Password: "))
+        p = input("Enter Password: ")
         if u == username and p == password:
             print("\033[92mLogin Successfully\033[0m")
             break
         else:
-            print("\033[91mIncorrect, Try again...\033[0m") 
-   
+            print("\033[91mIncorrect, Try again...\033[0m")
+def main_router():
+    manage = Manage_Medicine()
+    sale = Sale()
+    login()
     while True:
         print("\n======================= PHARMACY POS SYSTEM =========================")
-        print("1. Manage Products.")
-        print("2. Sales.")
-        print("3. Exit.")
+        print("1. Manage Products")
+        print("2. Sales")
+        print("3. Exit")
         choice = input("Choose Option: ")
         if choice == "1":
-            manage = Manage_Medicine()
             manage.menu()
         elif choice == "2":
             sale.menu(manage)
-
         elif choice == "3":
-            print("\033[91mExit Programming, Thanks....\033[0m")
+            print("\033[91mExit Program, Thanks...\033[0m")
             break
         else:
             print("\033[93mInvalid option. Try again.\033[0m")
-
 
 if __name__ == "__main__":
     main_router()
